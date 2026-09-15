5b4cfa18f7922e270fe34b214228202d7be9e2b8
AldoTrita <aldotrita@gmail.com>
2026-09-15 16:39:09 +0700

    Delete BAB 6 PYTHON directory

--- BAB 6 PYTHON/fungsi_dasar.py
+++
@@ -1,8 +0,0 @@
-# Contoh fungsi tanpa parameter dan tanpa retrun
-def salam_pembuka():
-    print("=" * 30)
-    print("    SELAMAT DATANG  ")
-    print("=" * 30)
-# Memanggil fungsi — bisa dipanggil berkali-kali
-salam_pembuka()
-salam_pembuka()
